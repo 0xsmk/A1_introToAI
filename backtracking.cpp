c95549d3ea6e77f556724f8edca20ab224f6699d
ShumakovMikhail <msumakov366@gmail.com>
2025-10-28 17:56:16 +0400

    Add another global variable and done all except backtrack alghoritm

--- backtracking.cpp
+++ backtracking.cpp
@@ -13,6 +13,10 @@ const int SIZE = 13;
 const int dx[4] = {1, -1, 0, 0};
 const int dy[4] = {0, 0, 1, -1};
 
+bool hasRing = true;
+bool ringOn = false;
+bool hasCoat = false;
+
 int perceptionVariant;               
 pair<int, int> gollum = {-1, -1};
 pair<int, int> mountDoom = {-1, -1};
@@ -25,8 +29,9 @@ bool inBounds(int x, int y); //Check if coordinates within the map
 void markDanger(int x, int y, char type); //mark cell like dangerous
 void readPerception(); // Reads the response from the interactor after the movement
 void moveTo(int x, int y);
+void putRingOn();
+void removeRing();
 bool backtrack(int x, int y);
-
 
 struct Cell{
     bool safe = true;
@@ -39,3 +44,142 @@ Cell grid[SIZE][SIZE];
 int main(){
 
 }
+
+
+bool inBounds(int x, int y){
+    return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
+}
+
+void markDanger(int x, int y, char type){
+    if (inBounds(x, y) == false) return;
+
+    int radius = 0;
+    bool moore = false;
+
+        if (type == 'O') { // Orc Patrol
+        radius = (hasCoat || ringOn) ? 0 : 1;
+        moore = false;
+    }
+    else if (type == 'U') { // Uruk-hai
+        radius = (hasCoat || ringOn) ? 1 : 2;
+        moore = false;
+    }
+    else if (type == 'N') { // Nazgûl
+        if (ringOn) radius = 2; else radius = 1;
+        moore = true;
+    }
+    else if (type == 'W') { // Watchtower
+        radius = 2;
+        moore = true;
+    }
+
+    grid[x][y].safe = false;
+    grid[x][y].type = type;
+
+    //  (Von Neumann)
+    if (!moore) {
+        for (int i = 1; i <= radius; i++) {
+
+            // down
+            if (inBounds(x + i, y)) {
+                grid[x + i][y].safe = false;
+                grid[x + i][y].type = type;
+            }
+
+            // up
+            if (inBounds(x - i, y)) {
+                grid[x - i][y].safe = false;
+                grid[x - i][y].type = type;
+            }
+
+            // right
+            if (inBounds(x, y + i)) {
+                grid[x][y + i].safe = false;
+                grid[x][y + i].type = type;
+            }
+
+            // left
+            if (inBounds(x, y - i)) {
+                grid[x][y - i].safe = false;
+                grid[x][y - i].type = type;
+            }
+        }
+    }
+
+    else {
+        for (int i = -radius; i <= radius; i++) {
+            for (int j = -radius; j <= radius; j++) {
+                int nx = x + i, ny = y + j;
+                if (inBounds(nx, ny)) {
+                    grid[nx][ny].safe = false;
+                    grid[nx][ny].type = type;
+                }
+            }
+        }
+    }
+}
+
+
+
+void readPerception(){
+    int n;
+    if (!(cin >> n)) exit(0); 
+
+    for (int i = 0; i < n; i++) {
+        int x, y; char t;
+        cin >> x >> y >> t;
+
+        if (t == 'O' || t == 'U' || t == 'N' || t == 'W')
+            markDanger(x, y, t);
+
+        if (t == 'P')
+            if (inBounds(x, y)) grid[x][y].safe = false;
+
+        // Gollum
+        if (t == 'G') {
+            gollum = {x, y};
+        }
+
+        // Mount Doom
+        if (t == 'M') {
+            mountDoom = {x, y};
+        }
+
+        // Mithril Coat
+        if (t == 'C') {
+            hasCoat = true;
+        }
+    }
+}
+
+void moveTo(int x, int y) {
+    cout << "m " << x << " " << y << endl;
+    cout.flush();
+    readPerception();
+
+    if (cin.peek() == 'M') {
+        string tmp;
+        cin >> tmp >> tmp >> tmp >> tmp >> mountDoom.first >> mountDoom.second;
+        foundGollum = true;
+    }
+}
+
+void putRingOn() {
+    if (!ringOn) {
+        cout << "r" << endl;
+        cout.flush();
+        ringOn = true;
+        readPerception();
+    }
+}
+
+
+void removeRing() {
+    if (ringOn) {
+        cout << "rr" << endl;
+        cout.flush();
+        ringOn = false;
+        readPerception();
+    }
+}
+
